0cd1b059a0723f2beb23da39c455d667ad20d58d
Fyrexian <vaethconner@gmail.com>
2024-04-25 11:47:27 -0500

    More projects optimized

--- Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,18 @@
 function writeFunction() {
     document.write(typeof "Word" + "<br>");
+    document.write(-3E310);
+    document.write(10 > 2);
+    console.log(10 + 5);
+    document.write(10 + 5);
+    document.write("10" + 5);
+    document.write(10 == 10);
+    document.write(3 == 11);
+    document.write(3 === 3);
+    document.write(3 === "3");
+
+    document.write(5 > 2 && 10 > 4);
+    document.write(5 > 10 && 10 > 4);
+    document.write(5 > 10 || 10 > 4);
+    document.write(5 > 10 || 10 > 20);
+    document.write(!(5 > 10));
  }
